b84b7e642eeea62cefdceee34f5c3dbe57f00d0b
parambehera <paramjyotibehera2005@gamil.com>
2025-07-05 19:02:26 +0530

    .

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config(); // Load environment variables from .env file
 
 const app = express();
 const server = http.createServer(app);
